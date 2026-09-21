78566f1e67dd224ac21d1447faecb77ae2ab165b
Sanskar Rai <sanskarr6812@gmail.com>
2026-09-21 22:04:18 +0545

    feat: add websocket server

--- backend/src/main.cpp
+++ backend/src/main.cpp
@@ -1,7 +1,19 @@
+#include "httplib.h"
 #include <iostream>
 
 int main() {
-    std::cout << "Hello World";
+    httplib::Server svr;
+
+    svr.WebSocket("/ws", [](const httplib::Request &, httplib::ws::WebSocket &ws) {
+        std::string msg;
+        while (ws.read(msg)) {
+            std::cout << msg << std::endl;
+            ws.send(msg);
+        }
+    });
+
+    std::cout << "Listening on port 8080..." << std::endl;
+    svr.listen("0.0.0.0", 8080);
 
     return 0;
 }
