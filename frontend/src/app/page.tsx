f4489581cfe1265e90748f1348a3b917104f3565
Ayush Shrestha <shresthaaayush863@gmail.com>
2026-09-23 06:31:12 +0545

    initialize web socket in client

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,41 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
+import axios from "axios";
+import { useEffect } from "react";
+
+export default async function Home() {
+  const ws = new WebSocket("ws://localhost:8080/ws")
+  useEffect(() => {
+
+    ws.onopen = () => {
+      console.log("Connection established");
+      ws.send("Calling from Next js")
+      
+    }
+
+    ws.onmessage = (event) => {
+      console.log("From the server: ", event.data);
+
+    }
+
+    ws.onerror = (error) => {
+      console.error("WebSocket error:", error);
+    };
+    ws.onclose = (event) => {
+      console.log("Connection closed");
+      console.log("Code:", event.code);
+      console.log("Reason:", event.reason);
+    };
+
+    
+  })
+
+
+
   return (
-   <>
-   <div className="flex items-center justify-center gap-10">
-    <button>Join room</button>
-    <button>Create room</button>
-   </div>
-   </>
+    <div className="flex items-center justify-center gap-10">
+      <button>Join room</button>
+      <button>Create room</button>
+    </div>
   );
 }
