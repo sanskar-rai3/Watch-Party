66b8bddaa4300e2ce52994c8cfa6ee31e96ee0f5
Ayush Shrestha <shresthaaayush863@gmail.com>
2026-09-23 06:30:44 +0545

    feat: add websocket server

--- backend/include/httplib.h
+++ backend/include/httplib.h
@@ -6251,8 +6251,10 @@ inline bool mmap::open(const char *path) {
   if (wpath.empty()) { return false; }
 
   hFile_ =
-      ::CreateFile2(wpath.c_str(), GENERIC_READ,
-                    FILE_SHARE_READ | FILE_SHARE_WRITE, OPEN_EXISTING, NULL);
+      ::CreateFileW(wpath.c_str(), GENERIC_READ,
+                             FILE_SHARE_READ, nullptr, OPEN_EXISTING,
+                             FILE_ATTRIBUTE_NORMAL, nullptr);
+
 
   if (hFile_ == INVALID_HANDLE_VALUE) { return false; }
 
