242024f58b3d773503f3fa0068758d8fca2e1d63
Sanskar Rai <sanskarr6812@gmail.com>
2026-09-26 13:21:21 +0545

    chore: change writing style

--- backend/src/api/partyApi.cpp
+++ backend/src/api/partyApi.cpp
@@ -5,44 +5,32 @@
 
 using json = nlohmann::json;
 
-void PartyServer::registerRoutes(httplib::Server &svr)
-{
-    svr.Post("/api/party/create",
-             [](const httplib::Request &req,
-                httplib::Response &res)
-             {
-                 try
-                 {
-                     json data = json::parse(req.body);
+void PartyServer::registerRoutes(httplib::Server &svr) {
+    svr.Post("/api/party/create", [](const httplib::Request &req, httplib::Response &res) {
+        try {
+            json data = json::parse(req.body);
 
-                     std::string host = data.at("host").get<std::string>();
-                     std::string partyName = data.at("partyName").get<std::string>();
-                     int maxUsers = data.at("maxUsers").get<int>();
+            std::string host = data.at("host").get<std::string>();
+            std::string partyName = data.at("partyName").get<std::string>();
+            int maxUsers = data.at("maxUsers").get<int>();
 
-                     std::cout << "Host: " << host << "\n";
-                     std::cout << "Party: " << partyName << "\n";
-                     std::cout << "Max users: " << maxUsers << "\n";
+            std::cout << "Host: " << host << "\n";
+            std::cout << "Party: " << partyName << "\n";
+            std::cout << "Max users: " << maxUsers << "\n";
 
-                     json response = {
-                         {"success", true},
-                         {"message", "Party craeted successfully"}};
+            json response = {
+                                {"success", true},
+                                {"message", "Party craeted successfully"}
+                            };
 
-                     res.set_content(
-                         response.dump(),
-                         "application/json");       
-                 }
-                 catch (const std::exception &e)
-                 {
-                     std::cerr << e.what() << '\n';
-                 }
-             });
+            res.set_content(response.dump(), "application/json");
+        }
+        catch (const std::exception &e) {
+            std::cerr << e.what() << '\n';
+        }
+    });
 
-    svr.Get("/api/parties",
-            [](const httplib::Request &req,
-               httplib::Response &res)
-            {
-                res.set_content(
-                    R"({"parties":[]})",
-                    "application/json");
-            });
+    svr.Get("/api/parties", [](const httplib::Request &req, httplib::Response &res) {
+        res.set_content(R"({"parties":[]})", "application/json");
+    });
 }
